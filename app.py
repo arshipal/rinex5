defda1863392a3b19d8e618852ca1c0d79a83b2a
arshipal <110916477+arshipal@users.noreply.github.com>
2022-08-15 17:31:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ ip = st.text_input('enter your text')
 op = model_nb.predict([ip])
 if st.button('PREDICT'):
   st.title(op[0])
-  
+             
                 
   
-                         
-    
+                           
+                                
              
     
     

a344657bdc05991ad50f5676500bf59f83c27364
arshipal <110916477+arshipal@users.noreply.github.com>
2022-08-15 17:42:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,15 @@ op = model_nb.predict([ip])
 if st.button('PREDICT'):
   st.title(op[0])
              
+             
                       
   
-                           
+          
+    
+    
+    
+    
+               
                                 
              
            
